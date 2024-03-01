c0c1d43c2bce0a4b08bd460cbe9a0ef217585928
pylypvl <86280296+pylypvl@users.noreply.github.com>
2024-03-01 01:44:09 -0600

    Update deploy_argocd.tf

--- deploy_argocd.tf
+++ deploy_argocd.tf
@@ -10,5 +10,5 @@ module "argocd_dev" {
 #   source             = "./terraform_argocd_root_eks"
 #   eks_cluster_name   = "gamania-dev"
 #   git_source_path    = "demo-dev/applications"
-#   git_source_repoURL = "git@github.com:pylypvl/helm.git"
+#   git_source_repoURL = "git@github.com:pylypvl/Helm-charts-examples.git"
 # }
